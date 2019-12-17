cae052f39378055563eeb805c2c4dd9772ae5e26
Robert Burris <roblburris@gmail.com>
2019-12-16 21:04:06 -0800

    removed the class, added one test command that does not seem to work

--- bot.py
+++ bot.py
@@ -1,28 +1,29 @@
+import asyncio
 import discord
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix=';')
+token = 'null'
 
 
-class RunBot:
-    def __init__(self, token):
-        self.token = token
+@bot.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(bot))
 
-    @bot.event()
-    async def on_ready(self):
-        print('We have logged in as {0.user}'.format(bot))
 
-    @bot.event
-    async def on_message(self, message):
-        curmessage = message.content.lower()
+@bot.event
+async def on_message(message):
+    curmessage = message.content.lower()
 
-        if "hello" in curmessage and message.author != bot.user:
-            await message.channel.send('Hello!')
-            print("sent hello!")
+    if "hello" in curmessage and message.author != bot.user:
+        await message.channel.send('Hello!')
+        print("sent hello!")
 
-    @bot.command()
-    async def test(self, ctx, arg):
-        print('hi')
 
-    async def runbot(self):
-        bot.run(self.token)
+@bot.command()
+async def test(ctx):
+    await ctx.send("Hello World!")
+    print('hi')
+
+
+bot.run(token)
