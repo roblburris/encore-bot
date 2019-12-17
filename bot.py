61f987b8d150713b91b59623aa6d8768a78beeb5
Robert Burris <roblburris@gmail.com>
2019-12-16 21:20:25 -0800

    removed class defining bot, fixed commands, added preliminary comments
    Signed-off-by: Robert Burris <roblburris@gmail.com>

--- bot.py
+++ bot.py
@@ -1,5 +1,3 @@
-import asyncio
-import discord
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix=';')
@@ -11,6 +9,7 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
 
 
+# parses messages to see if a user's message contains 'hello' in it and if it does, replies with 'Hello!' back
 @bot.event
 async def on_message(message):
     curmessage = message.content.lower()
@@ -19,11 +18,20 @@ async def on_message(message):
         await message.channel.send('Hello!')
         print("sent hello!")
 
+    await bot.process_commands(message)
 
-@bot.command()
+
+# defines the test command
+@bot.command(pass_context=True)
 async def test(ctx):
     await ctx.send("Hello World!")
-    print('hi')
+    print('sent \'Hello World!\'')
+
+# defines the repeat command
+@bot.command()
+async def repeat(ctx, arg):
+    await ctx.send(arg)
+    print('sent \'' + arg + '\'')
 
 
 bot.run(token)
